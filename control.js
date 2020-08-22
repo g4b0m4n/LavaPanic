316e13dfc547296cc44b87a823099893050f4cc7
g4b0m4n <g4b0m3ndoz@protonmail.com>
2020-08-22 01:57:18 -0300

    beta

--- control.js
+++ control.js
@@ -1,4 +1,4 @@
-var Ngotas,frames=0,change_tile=false, context, controller, level = 1, loop, width, height;
+var gout = null, splat = null, Ngotas = 0, frames = 0, change_tile = false, context, controller, level = 1, loop, width, height;
 
 context = document.querySelector("canvas").getContext("2d");
 
@@ -41,7 +41,7 @@ var empty = "empty_bucket";
 
 class Steve {
 
-    constructor(x,y,w,h) {
+    constructor(x, y, w, h) {
         this.tile = "steve_stand";
         this.jumping = false;
         this.health = 10;
@@ -54,9 +54,9 @@ class Steve {
         this.last_left = false;
     }
 }
-let steve = new Steve(200,230,100,170);
-let steve2 = new Steve(200,530,100,170);
-console.log(steve.tile +"\n"+ steve.x +"\n"+ steve.last_left);
+let steve = new Steve(200, 230, 100, 170);
+let steve2 = new Steve(200, 530, 100, 170);
+//console.log(steve.tile +"\n"+ steve.x +"\n"+ steve.last_left);
 
 class Bucket {
 
@@ -66,29 +66,30 @@ class Bucket {
         this.y = 0;
         this.w = 50;
         this.h = 50;
+        this.y_velocity = 1;
         this.tile = empty;
         this.bucket_full = false;
         this.health = 10;
     }
     setxl(value) {
-            bucket.xl  = value-30;
-        }
+        bucket.xl = value-30;
+    }
     setxr(value) {
-            bucket.xr  = value+80;
-        }
+        bucket.xr = value+80;
+    }
     sety(value) {
-            bucket.y  = value+70;
-        }
-        
+        bucket.y = value+70;
+    }
+
     getxl() {
-            return bucket.xl;
-        }
+        return bucket.xl;
+    }
     getxr() {
-            return bucket.xr;
-        }
+        return bucket.xr;
+    }
     gety() {
-            return bucket.y;
-        }
+        return bucket.y;
+    }
 }
 bucket = new Bucket();
 bucket2 = new Bucket();
@@ -106,10 +107,9 @@ class Gout {
 
     colision(object) {
 
-        if (object.x > this.x && object.y > this.y
-            && object.h < this.h && object.w < this.w) {
+        if (object.w > this.x && object.h > this.y ||
+            object.y < this.h && object.x < this.w) {
 
-            object.health--;
             //play sound
             return true;
         } else {
@@ -117,30 +117,6 @@ class Gout {
         }
     }
 }
-let gout;
-var goutPlop = function() {
-    
-    setTimeout(() => {
-    if (gout == null){
-        gout = new Gout();
-        console.log("# gotas: "+Ngotas);
-        //velocity aka level
-        //document.getElementById("level").setAttribute("value", level);
-        //level = document.getElementById("level").value;
-        //color
-        context.fillStyle = "#FF4500";
-        //shape
-        context.fillRect(gout.x, gout.y, gout.w, gout.h);
-        gout.y += Number(level);
-        
-    } else if (gout.colision(steve)
-    | gout.colision(bucket) | gout.y > 400) {
-        gout = null;
-        console.log("gout dissapear");
-    }},
-        1000-(level*100));
-};
-
 controller = {
 
     left: false,
@@ -150,7 +126,6 @@ controller = {
     touchListener: function(event) {
         event.preventDefault();
         var touch_state = event.type == "touchstart"?true:false;
-        
         switch (event.touches[0].pageX < 250) {
             case true:
                 controller.left = touch_state;
@@ -162,8 +137,8 @@ controller = {
         if (event.touches[0].pageY < 200) {controller.up = touch_state;}
     },*/
     mouseListener: function(event) {
-        var touch_state = event.type == "mousedown"?true:false;
-        
+        var touch_state = event.type == "mousedown"?true: false;
+
         switch (event.clientX < 250) {
             case true:
                 controller.left = touch_state;
@@ -172,7 +147,9 @@ controller = {
                 controller.right = touch_state;
                 break;
         }
-        if (event.clientY < 200) {controller.up = touch_state;}
+        if (event.clientY < 200) {
+            controller.up = touch_state;
+        }
     },
     keyListener: function() {
         var key_state = event.type == "keydown"?true: false;
@@ -190,11 +167,11 @@ controller = {
                 break;
         }
     }/*keyListener end*/
-};/*controller end*/
+}; /*controller end*/
 
 loop = function() {
 
-    //background
+    //background & hearts
     for (let i = 0; i < width/100; i++) {
         context.drawImage(document.getElementById(magma_block), i*100, 0, 100, 100);
         context.drawImage(document.getElementById(cora), 10+steve.health*(5*i), 10, 50, 50);
@@ -203,9 +180,11 @@ loop = function() {
             context.drawImage(document.getElementById(stone_block), i*100, s*100, 100, 100);
         }
     }
+    //score
     context.font = "30px Courier New bold";
     context.fillStyle = "#ffffff";
     context.fillText("capacity: "+(10-bucket.health)+"/10", width-200, 50);
+
     //bucket
     if (bucket.health == 0) {
         bucket.tile = full;
@@ -215,20 +194,21 @@ loop = function() {
     bucket.setxl(steve.x);
     bucket.setxr(steve.x);
     bucket.sety(steve.y);
-    
+
     if (steve.last_left) {
         context.drawImage(document.getElementById(bucket.tile), bucket.xl, bucket.y, 50, 50);
     } else {
         context.drawImage(document.getElementById(bucket.tile), bucket.xr, bucket.y, 50, 50);
     }
-    
-    //steve 
+
+    //steve
     steve.y_velocity += 1.5; // gravity
     steve.x += steve.x_velocity;
     steve2.x += steve2.x_velocity;
     steve.y += steve.y_velocity;
     steve.x_velocity *= 0.9; // friction
     steve.y_velocity *= 0.9; // friction
+    gout?gout.y+=2:false;
 
     //tile
     context.drawImage(document.getElementById(steve.tile), steve.x, steve.y, steve.w, steve.h);
@@ -238,44 +218,46 @@ loop = function() {
         steve.y += steve.y_velocity;
         steve.jumping = true;
     }
-    
+
     if (controller.left) {
         steve.x_velocity += -0.5;
         steve.x += steve.x_velocity;
         steve.last_left = true;
-        
-        if (frames%10==0){
+
+        if (frames%10 == 0) {
             if (change_tile) {
-                change_tile=false;
-            }else {change_tile=true;
-                }
+                change_tile = false;
+            } else {
+                change_tile = true;
+            }
         }
         if (change_tile) {
             steve.tile = steve_stand_left;
             steve2.tile = steve_stand_left;
-        }else {
+        } else {
             steve.tile = steve_walk_left;
             steve2.tile = steve_walk_left;
-            }
+        }
 
     } else if (controller.right) {
         steve.x_velocity += 0.5;
         steve.x += steve.x_velocity;
         steve.last_left = false;
-        
-        if (frames%10==0){
+
+        if (frames%10 == 0) {
             if (change_tile) {
-                change_tile=false;
-            }else {change_tile=true;
-                }
+                change_tile = false;
+            } else {
+                change_tile = true;
+            }
         }
         if (change_tile) {
             steve.tile = steve_stand;
             steve2.tile = steve_stand;
-        }else {
+        } else {
             steve.tile = steve_walk;
             steve2.tile = steve_walk;
-            }
+        }
     } else if (steve.last_left) {
         steve.tile = steve_stand_left;
         steve2.tile = steve_stand_left;
@@ -302,17 +284,18 @@ loop = function() {
         steve.x = 400;
         bucket.bucket_full = false;
 
-    }
-    else if (steve2.x > 400 || steve2.x < 0) {
-        
+    } else if (steve2.x > 400 || steve2.x < 0) {
+
         let current = steve2.x;
-        setTimeout(() => {steve2.x = current;}, 5000);
+        setTimeout(() => {
+            steve2.x = current;
+        }, 5000);
         steve2.x_velocity *= -1;
         steve2.x += steve.x_velocity;
-        
-        if(bucket.bucket_full)
-            bucket2.bucket_full=
-                bucket2.bucket_full?true:false;
+
+        if (bucket.bucket_full)
+            bucket2.bucket_full =
+        bucket2.bucket_full?true: false;
 
     }
 
@@ -321,7 +304,7 @@ loop = function() {
 
         steve.y = 100;
     }
-    
+
     //bucket2
     if (bucket2.health == 0) {
         bucket2.tile = full;
@@ -331,31 +314,67 @@ loop = function() {
     bucket2.setxl(steve2.x);
     bucket2.setxr(steve2.x);
     bucket2.sety(steve2.y);
-    
+
     if (steve2.last_left) {
-        context.drawImage(document.getElementById(bucket2.tile), bucket2.xl, bucket2.y, 50, 50);
+        context.drawImage(document.getElementById(bucket2.tile), bucket2.getxl, bucket2.gety, 50, 50);
     } else {
-        context.drawImage(document.getElementById(bucket2.tile), bucket2.xr, bucket2.y, 50, 50);
+        context.drawImage(document.getElementById(bucket2.tile), bucket2.getxr, bucket2.gety, 50, 50);
     }
-    
+
     //steve2
     //tile
     context.drawImage(document.getElementById(steve2.tile), steve2.x, steve2.y, steve2.w, steve2.h);
-    
-    goutPlop();
-    
-    console.log(steve.health+"<br>"+bucket.health)
-    // call update when the browser is ready to draw again
-    setInterval(window.requestAnimationFrame(loop), 66);
-    frames++;
-}//loop
 
-window.addEventListener("mousedown", controller.mouseListener,false);
-window.addEventListener("mouseup", controller.mouseListener,false);
-window.addEventListener("touchstart", controller.touchListener,false);
-window.addEventListener("touchend", controller.touchListener,false);
-window.addEventListener("touchcancel", controller.touchListener,false);
-//window.addEventListener("touchmove", controller.touchListener,false);
-window.addEventListener("keydown", controller.keyListener);
-window.addEventListener("keyup", controller.keyListener);
-window.requestAnimationFrame(loop); //draw first frame
+    //gout
+
+    //velocity aka level
+    //document.getElementById("level").setAttribute("value", level);
+    //level = document.getElementById("level").value;
+    //color
+    context.fillStyle = "#FF4500";
+    //splat draw
+    if (Ngotas>1) {
+        setTimeout(context.fillRect(splat-50,
+            398, 100, 2), 2000);
+        if (steve.x > splat-50 &&
+        steve.x < splat+50)steve.health--;
+    }
+        if (gout) {
+            context.fillRect(gout.x, gout.y,
+            gout.w, gout.h); console.log("draw");
+            console.log(gout.x+"x"+gout.y);
+            //colision //gout true
+            if (gout.colision(steve) || gout.colision(bucket)) {
+                bucket.health-=1;steve.health-=1;
+            } else if (gout.y > 400) {
+                splat = gout.x;
+                console.log("splat");
+                gout = null;
+            }
+        //creation
+       } else {
+            gout = new Gout(); console.log("gout");
+        }
+        //console.log(steve.health+"<br>"+bucket.health
+        if (steve.health==0){
+            context.fillStyle = "#000000";
+            context.fillRect(0,0,width,height);
+            context.fillStyle = "#330000";
+            context.font = "20px Courier New bold";
+            context.fillText("You died!", width/2, height/2);
+        } else {
+        // call update when the browser is ready to draw again
+        setInterval(window.requestAnimationFrame(loop), 66);
+        frames++;
+        }
+    }//loop
+
+    window.addEventListener("mousedown", controller.mouseListener, false);
+    window.addEventListener("mouseup", controller.mouseListener, false);
+    window.addEventListener("touchstart", controller.touchListener, false);
+    window.addEventListener("touchend", controller.touchListener, false);
+    window.addEventListener("touchcancel", controller.touchListener, false);
+    //window.addEventListener("touchmove", controller.touchListener,false);
+    window.addEventListener("keydown", controller.keyListener);
+    window.addEventListener("keyup", controller.keyListener);
+    window.requestAnimationFrame(loop); //draw first frame
